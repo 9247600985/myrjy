a16a239c333804bfd555522234a9aa0114cb4867
lokesh <lokesh8278@gmail.com>
2022-01-25 12:30:17 +0530

    updated

--- src/app/user/user.routing.ts
+++ src/app/user/user.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { CovidComponent } from './covid/covid.component';
 import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
 import { NewsComponent } from './news/news.component';
 import { ToolsComponent } from './tools/tools.component';
 import { TourcardComponent } from './tourcard/tourcard.component';
@@ -23,6 +24,9 @@ const routes: Routes = [
         path:'covid', component: CovidComponent
       },
       {
+        path:'job', component: JobComponent
+      },
+      {
         path:'home', component: HomeComponent,
         children: [
           {
@@ -33,6 +37,15 @@ const routes: Routes = [
           },
           {
             path:'tools', component: ToolsComponent
+          },
+          {
+            path:'tools', component: ToolsComponent
+          },
+          {
+            path:'covid', component: CovidComponent
+          },
+          {
+            path:'job', component: JobComponent
           },
         ]
       }
